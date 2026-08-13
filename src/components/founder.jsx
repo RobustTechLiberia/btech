048c7623ae88d4a87533cd7cca0b44b36d9661a5
Gabriel_31997 <gabrielwkun@gmail.com>
2026-08-13 11:31:26 +0000

    deploy

--- src/components/founder.jsx
+++ src/components/founder.jsx
@@ -5,13 +5,13 @@ class MeetFounder extends React.Component {
   render() {
     return (
       <>
-        <div className="flex flex-wrap justify-start md:mt-0 md:gap-5 lg:gap-5 lg:mt-0 mt-0 md:mx-5 lg:mx-5 mx-0 h-96 bg-white">
+        <div className="flex flex-wrap justify-start md:mt-0 md:gap-0 lg:gap-0 lg:mt-0 mt-0 md:mx-5 lg:mx-5 mx-0 h-96 bg-white">
           <div className="w-auto">
             <div className="md:w-3xl lg:w-3xl h-auto object-cover">
               <img src={founder} alt="" className="w-auto h-auto" />
             </div>
           </div>
-          <div className="md:w-xl lg:w-xl">
+          <div className="md:w-xl lg:w-xl bg-gray-100">
             <h1 className="lg:text-center md:text-center text-center md:pt-50 lg:pt-50 mt-10 md:mx-18 lg:mx-18 text-5xl  font-sans md:text-5xl lg:text-5xl text-gray-900 font-bold capitalize">
               meet the founder
               <br />
