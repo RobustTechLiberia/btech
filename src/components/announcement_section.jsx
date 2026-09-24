2dccffeee4c5c5db8ea06475e3a396bad3e3ad12
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-24 09:19:00 +0000

    deployment

--- src/components/announcement_section.jsx
+++ src/components/announcement_section.jsx
@@ -4,15 +4,21 @@ class Announcement extends React.Component {
   render() {
     return (
       <>
-        <h1 className="font-sans font-bold text-4xl text-center capitalize text-[#253C6D]">
-          why choose btech?
-        </h1>
-        <div className="flex justify-center items-center bg-white md:gap-20 gap-10 mt-20">
-          {/* reasons */}
-          <div className="w-80 h-80 shadow-sm border border-gray-200"></div>
-          <div className="w-80 h-80 shadow-sm border border-gray-200"> </div>
-          <div className="w-80 h-80 shadow-sm border border-gray-200"></div>
-        </div>
+        <div className="h-auto bg-gray-20">
+          <div className="flex flex-wrap justify-center items-center">
+            <h1 className="font-sans font-bold text-4xl text-center md:mt-20 mt-10 capitalize text-[#253C6D]">
+              why choose btech?
+            </h1>
+          </div>
+          <div className="flex justify-center items-center md:gap-20 gap-10 mt-20">
+            {/* reasons */}
+            <div className="w-80 h-80 shadow-sm border bg-white border-gray-200"></div>
+            <div className="w-80 h-80 shadow-sm border bg-white border-gray-200">
+              {" "}
+            </div>
+            <div className="w-80 h-80 shadow-sm border bg-white border-gray-200"></div>
+          </div>
+        </div>{" "}
       </>
     );
   }
